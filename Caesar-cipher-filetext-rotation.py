46341e4eb2c4e7b68d176a30114e8cca64e8eb2e
Subham Lamichhane <lamichhanesubham4@gmail.com>
2025-06-03 00:21:04 -0700

    Update Caesar-cipher-filetext-rotation.py
    Added functionality to either keep or remove the main file while keeping encrypted file !!

--- Caesar-cipher-filetext-rotation.py
+++ Caesar-cipher-filetext-rotation.py
@@ -1,4 +1,6 @@
 import time
+import os
+#This will prompt user either to keep or remove orginal file
 filename=input("Enter the name of file or provide full path if raised error :").strip(' " ')
 shift=int(input("Enter the shift value also :"))
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -13,10 +15,26 @@ with open (fr'{filename}','r') as file :
             output_message+=alphabet[new_location]
         else :
             output_message+=x
-askuser=input("Enter the name of the rotated file you wish to save as : ")
-print("Please wait !! Just a sec .......")
-time.sleep(3)
-print("The rotated file has been saved in Download directory !! ")
-with open (fr"C:\Users\Dell\Downloads\{askuser}","w") as file1 :
-    file1.write(output_message)
-print("Saved successfully !!")
+
+#asking users either to keep or delete the orginal file !!
+ask_user=input('"Press "k" to keep the orginal file/ "D for deleting the orginal file').lower()
+if ask_user=="k":
+    askuser=input("Enter the name of the rotated file you wish to save as : ")
+    print("Please wait !! Just a sec .......")
+    time.sleep(3)
+    print("The rotated file has been saved in Download directory !! ")
+    with open (fr"C:\Users\Dell\Downloads\{askuser}","w") as file1 :
+        file1.write(output_message)
+    print("Saved successfully !!")
+if ask_user=="d":
+    os.remove(filename)
+    print("Wait !! The original file is being removed from the system !")
+    time.sleep(3)
+    askuser=input("Enter the name of the rotated file you wish to save as : ")
+    print("Please wait !! Just a sec .......")
+    time.sleep(3)
+    print("The rotated file has been saved in Download directory !! ")
+    with open (fr"C:\Users\Dell\Downloads\{askuser}","w") as file1 :
+        file1.write(output_message)
+else :
+    print('Select either "k" or "D"!! dumb idiot ')
